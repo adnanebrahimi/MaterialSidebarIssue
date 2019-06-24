ebaab133a94b2f2b7db6689abfb91c49e039f8bb
Adnan Ebrahimi <adnan.ebrahimi@gmail.com>
2019-06-24 13:23:22 +0430

    Init 🚀

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SidebarLayoutComponent } from './sidebar-layout/sidebar-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'page-one', pathMatch: 'full' },
+  {
+    path: '',
+    component: SidebarLayoutComponent,
+    children: [
+      {
+        path: 'page-one',
+        loadChildren: () => import('./page-one/page-one.module').then(m => m.PageOneModule)
+      },
+      {
+        path: 'page-two',
+        loadChildren: () => import('./page-two/page-two.module').then(m => m.PageTwoModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/page-two/page-two-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PageTwoComponent } from './page-two/page-two.component';
+
+const routes: Routes = [
+  { path: '', component: PageTwoComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PageTwoRoutingModule { }
